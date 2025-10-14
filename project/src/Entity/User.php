c0805e0f334bbe222d59b5514aa00e4a692936a0
Senya Senya <senyasenya010896@gmail.com>
2025-10-14 12:09:57 +0300

    refresh and access tokens

--- project/src/Entity/User.php
+++ project/src/Entity/User.php
@@ -25,14 +25,14 @@ use Symfony\Component\Security\Core\User\UserInterface;
         new Get()
     ]
 )]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_UUID', fields: ['uuid'])]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_UUID', fields: ['id'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator('doctrine.uuid_generator')]
     #[ORM\Column(type: UlidType::NAME, unique: true, nullable: false)]
-    private ?string $uuid = null;
+    private ?string $id = null;
 
     #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
@@ -49,14 +49,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    public function getUuid(): ?string
+    public function getId(): ?string
     {
-        return $this->uuid;
+        return $this->id;
     }
 
-    public function setUuid(string $uuid): static
+    public function setId(string $id): static
     {
-        $this->uuid = $uuid;
+        $this->id = $id;
 
         return $this;
     }
@@ -68,7 +68,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string)$this->uuid;
+        return (string)$this->id;
     }
 
     public function getEmail(): ?string
